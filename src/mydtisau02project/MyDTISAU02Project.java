aa77e67a9faf390d90c8db1dca87be0908396239
SorawitLeekjaroen <springtrap2009@gmail.com>
2025-01-19 18:14:04 +0700

    v2

---
+++ src/mydtisau02project/MyDTISAU02Project.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mydtisau02project;
+
+/**
+ *
+ * @author IT306PC
+ */
+public class MyDTISAU02Project {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
